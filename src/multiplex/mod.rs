fbc5b9e7dfe91677072a7973270f6ad96186ea97
Carl Lerche <me@carllerche.com>
2016-10-05 13:10:59 -0700

    Impl Transport for Box<Transport>

--- src/multiplex/mod.rs
+++ src/multiplex/mod.rs
@@ -241,3 +241,71 @@ impl<T, M1, M2, B1, B2, E> Transport for T
         FramedIo::flush(self)
     }
 }
+
+impl<M1, M2, B1, B2, E> Transport for Box<Transport<In = M1, Out = M2, BodyIn = B1, BodyOut = B2, Error = E>>
+    where E: From<Error<E>> + 'static,
+          M1: 'static,
+          M2: 'static,
+          B1: 'static,
+          B2: 'static,
+{
+    type In = M1;
+    type BodyIn = B1;
+    type Out = M2;
+    type BodyOut = B2;
+    type Error = E;
+
+    fn poll_read(&mut self) -> Async<()> {
+        (**self).poll_read()
+    }
+
+    fn read(&mut self) -> Poll<Frame<M2, B2, E>, io::Error> {
+        (**self).read()
+    }
+
+    fn poll_write(&mut self) -> Async<()> {
+        (**self).poll_write()
+    }
+
+    fn write(&mut self, req: Frame<M1, B1, E>) -> Poll<(), io::Error> {
+        (**self).write(req)
+    }
+
+    fn flush(&mut self) -> Poll<(), io::Error> {
+        (**self).flush()
+    }
+}
+
+impl<M1, M2, B1, B2, E> Transport for Box<Transport<In = M1, Out = M2, BodyIn = B1, BodyOut = B2, Error = E> + Send>
+    where E: From<Error<E>> + 'static,
+          M1: 'static,
+          M2: 'static,
+          B1: 'static,
+          B2: 'static,
+{
+    type In = M1;
+    type BodyIn = B1;
+    type Out = M2;
+    type BodyOut = B2;
+    type Error = E;
+
+    fn poll_read(&mut self) -> Async<()> {
+        (**self).poll_read()
+    }
+
+    fn read(&mut self) -> Poll<Frame<M2, B2, E>, io::Error> {
+        (**self).read()
+    }
+
+    fn poll_write(&mut self) -> Async<()> {
+        (**self).poll_write()
+    }
+
+    fn write(&mut self, req: Frame<M1, B1, E>) -> Poll<(), io::Error> {
+        (**self).write(req)
+    }
+
+    fn flush(&mut self) -> Poll<(), io::Error> {
+        (**self).flush()
+    }
+}
