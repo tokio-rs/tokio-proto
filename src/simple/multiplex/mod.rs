280ade6339aa33c8b3577446701041c34201ef9f
Aaron Turon <aturon@mozilla.com>
2016-12-22 13:25:02 -0800

    Hook error frames into non-streaming protocols
    This commit refactors non-streaming protocols to allow for transports
    with error frames, and to properly hook that into dispatch.
    
    Closes #97

--- src/simple/multiplex/mod.rs
+++ src/simple/multiplex/mod.rs
@@ -2,6 +2,8 @@
 //!
 //! See the crate-level docs for an overview.
 
+use streaming::multiplex::Frame;
+
 mod client;
 pub use self::client::ClientProto;
 pub use self::client::ClientService;
@@ -18,101 +20,37 @@ pub type RequestId = u64;
 /// implement the `ClientProto` or `ServerProto` traits in this module.
 pub struct Multiplex;
 
-// This is a submodule so that `LiftTransport` can be marked `pub`, to satisfy
-// the no-private-in-public checker.
-mod lift {
-    use std::io;
-    use std::marker::PhantomData;
-
-    use super::RequestId;
-    use streaming::multiplex::{Frame, Transport};
-    use futures::{Future, Stream, Sink, StartSend, Poll, Async, AsyncSink};
-
-    // Lifts an implementation of RPC-style transport to streaming-style transport
-    pub struct LiftTransport<T, E>(pub T, pub PhantomData<E>);
-
-    // Lifts the Bind from the underlying transport
-    pub struct LiftBind<A, F, E> {
-        fut: F,
-        marker: PhantomData<(A, E)>,
+fn lift_msg_result<T, E>(msg: (RequestId, Result<T, E>)) -> Frame<T, (), E> {
+    let (id, msg) = msg;
+    match msg {
+        Ok(msg) => Frame::Message { message: msg, body: false, solo: false, id: id },
+        Err(err) => Frame::Error { error: err, id: id },
     }
+}
 
-    impl<T, InnerItem, E> Stream for LiftTransport<T, E> where
-        E: 'static,
-        T: Stream<Item = (RequestId, InnerItem), Error = io::Error>,
-    {
-        type Item = Frame<InnerItem, (), E>;
-        type Error = io::Error;
-
-        fn poll(&mut self) -> Poll<Option<Self::Item>, io::Error> {
-            let (id, msg) = match try_ready!(self.0.poll()) {
-                Some(msg) => msg,
-                None => return Ok(None.into()),
-            };
-            Ok(Some(Frame::Message {
-                message: msg,
-                body: false,
-                solo: false,
-                id: id,
-            }).into())
+fn lower_msg_result<T, E>(msg: Frame<T, (), E>) -> (RequestId, Result<T, E>) {
+    match msg {
+        Frame::Message { message, body, solo, id } => {
+            if !body && !solo { return (id, Ok(message)) }
         }
+        Frame::Error { error, id } => return (id, Err(error)),
+        _ => {}
     }
+    panic!("Encountered a streaming body in a non-streaming protocol")
+}
 
-    impl<T, InnerSink, E> Sink for LiftTransport<T, E> where
-        E: 'static,
-        T: Sink<SinkItem = (RequestId, InnerSink), SinkError = io::Error>
-    {
-        type SinkItem = Frame<InnerSink, (), E>;
-        type SinkError = io::Error;
+fn lift_msg<T, E>(msg: (RequestId, T)) -> Frame<T, (), E> {
+    Frame::Message { message: msg.1, body: false, solo: false, id: msg.0 }
+}
 
-        fn start_send(&mut self, request: Self::SinkItem)
-                      -> StartSend<Self::SinkItem, io::Error> {
-            if let Frame::Message { message, id, body, solo } = request {
-                if !body && !solo {
-                    match try!(self.0.start_send((id, message))) {
-                        AsyncSink::Ready => return Ok(AsyncSink::Ready),
-                        AsyncSink::NotReady((id, msg)) => {
-                            let msg = Frame::Message {
-                                message: msg,
-                                id: id,
-                                body: false,
-                                solo: false,
-                            };
-                            return Ok(AsyncSink::NotReady(msg))
-                        }
-                    }
-                }
-            }
-            Err(io::Error::new(io::ErrorKind::Other, "no support for streaming"))
+fn lower_msg<T, E>(msg: Frame<T, (), E>) -> (RequestId, T) {
+    match msg {
+        Frame::Message { message, body, solo, id } => {
+            if !body && !solo { return (id, message) }
         }
-
-        fn poll_complete(&mut self) -> Poll<(), io::Error> {
-            self.0.poll_complete()
-        }
+        Frame::Error { .. } =>
+            panic!("Encountered unexpected error frame in a non-streaming protocol"),
+        _ => {}
     }
-
-    impl<T, InnerItem, InnerSink, E> Transport<()> for LiftTransport<T, E> where
-        E: 'static,
-        T: 'static,
-        T: Stream<Item = (RequestId, InnerItem), Error = io::Error>,
-        T: Sink<SinkItem = (RequestId, InnerSink), SinkError = io::Error>
-    {}
-
-    impl<A, F, E> LiftBind<A, F, E> {
-        pub fn lift(f: F) -> LiftBind<A, F, E> {
-            LiftBind {
-                fut: f,
-                marker: PhantomData,
-            }
-        }
-    }
-
-    impl<A, F, E> Future for LiftBind<A, F, E> where F: Future<Error = io::Error> {
-        type Item = LiftTransport<F::Item, E>;
-        type Error = io::Error;
-
-        fn poll(&mut self) -> Poll<Self::Item, io::Error> {
-            Ok(Async::Ready(LiftTransport(try_ready!(self.fut.poll()), PhantomData)))
-        }
-    }
+    panic!("Encountered a streaming body in a non-streaming protocol");
 }
